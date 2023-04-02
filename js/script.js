409b2817c5a2bf3a91ada6fd96d4a27f2eb0d05e
omni23 <89713821+omni23@users.noreply.github.com>
2023-04-02 14:32:51 -0400

    Fix bug allowing user to enter a number with multiple decimals

--- js/script.js
+++ js/script.js
@@ -13,8 +13,12 @@ let total = 0;
 let operator = null;
 let firstOpReceived = false;
 
-numberButtons.forEach(button => button.addEventListener("click", updateOutput));
-operatorButtons.forEach(button => button.addEventListener("click", storeExpression));
+numberButtons.forEach((button) =>
+  button.addEventListener("click", updateOutput)
+);
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", storeExpression)
+);
 equalsButton.addEventListener("click", operate);
 clearButton.addEventListener("click", clearAll);
 undoButton.addEventListener("click", clear);
@@ -22,90 +26,101 @@ signButton.addEventListener("click", flipSign);
 decimalButton.addEventListener("click", addDecimal);
 
 function operate() {
-    if (!firstOpReceived) return;
+  if (!firstOpReceived) return;
 
-    let rightValue = Number(bottomOutput.textContent);
+  let rightValue = Number(bottomOutput.textContent);
 
-    switch (operator) {
-        case "+":
-            total = leftValue + rightValue;
-            break;
-        case "-":
-            total = leftValue - rightValue;
-            break;
-        case "x":
-            total = leftValue * rightValue;
-            break;
-        case "÷":
-            rightValue === 0 ? total = "Undefined" : total = leftValue / rightValue;
-    }
+  switch (operator) {
+    case "+":
+      total = leftValue + rightValue;
+      break;
+    case "-":
+      total = leftValue - rightValue;
+      break;
+    case "x":
+      total = leftValue * rightValue;
+      break;
+    case "÷":
+      rightValue === 0
+        ? (total = "Undefined")
+        : (total = leftValue / rightValue);
+  }
 
-    if (total.toString().length > 18) total = total.toExponential(4);
-    if(decimalButton.disabled) decimalButton.disabled = false;
+  if (total.toString().length > 18) total = total.toExponential(4);
+  if (decimalButton.disabled) decimalButton.disabled = false;
 
-    bottomOutput.textContent = total;
-    firstOpReceived = false;
+  bottomOutput.textContent = total;
+  firstOpReceived = false;
 }
 
 function storeExpression(event) {
-    if (firstOpReceived) return;
+  if (firstOpReceived) return;
 
-    operator = event.target.textContent;
-    leftValue = Number(bottomOutput.textContent);
-    bottomOutput.textContent = "0";
-    firstOpReceived = true;
-    if(decimalButton.disabled) decimalButton.disabled = false;
+  operator = event.target.textContent;
+  leftValue = Number(bottomOutput.textContent);
+  bottomOutput.textContent = "0";
+  firstOpReceived = true;
+  if (decimalButton.disabled) decimalButton.disabled = false;
 }
 
 function updateOutput(event) {
-    value = event.target.textContent;
+  value = event.target.textContent;
 
-    if ((bottomOutput.textContent + value).length > 18) {
-        topOutput.textContent = "Exceeded display length";
-        return;
-    }
+  if ((bottomOutput.textContent + value).length > 18) {
+    topOutput.textContent = "Exceeded display length";
+    return;
+  }
 
-    bottomOutput.textContent = bottomOutput.textContent === "0" ? value :
-        bottomOutput.textContent += value;
+  bottomOutput.textContent =
+    bottomOutput.textContent === "0"
+      ? value
+      : (bottomOutput.textContent += value);
 }
 
 function clear() {
-    let numbers = bottomOutput.textContent.match(/^\d*\.?\d*/).join('');
-    if (numbers.length > 1) {
-        if (numbers[numbers.length - 1] === ".") {
-            decimalButton.disabled = false;
-        }
-        let currentLength = bottomOutput.textContent.length;
-        let previousValue = bottomOutput.textContent.slice(0, currentLength - 1); 
-        bottomOutput.textContent = previousValue;
+  let numbers = bottomOutput.textContent.match(/^\d*\.?\d*/).join("");
+  if (numbers.length > 1) {
+    if (numbers[numbers.length - 1] === ".") {
+      decimalButton.disabled = false;
     }
-    else {
-        bottomOutput.textContent = "0";
-    }
+    let currentLength = bottomOutput.textContent.length;
+    let previousValue = bottomOutput.textContent.slice(0, currentLength - 1);
+    bottomOutput.textContent = previousValue;
+  } else {
+    bottomOutput.textContent = "0";
+  }
 }
 
 function clearAll() {
-    leftValue = 0;
-    total = 0;
-    bottomOutput.textContent = 0;
-    topOutput.textContent = "";
-    firstOpReceived = false;
-    if(decimalButton.disabled) decimalButton.disabled = false;
+  leftValue = 0;
+  total = 0;
+  bottomOutput.textContent = 0;
+  topOutput.textContent = "";
+  firstOpReceived = false;
+  if (decimalButton.disabled) decimalButton.disabled = false;
 }
 
 function flipSign() {
-    if (bottomOutput.textContent.includes("-")) {
-        let valueToFlip = bottomOutput.textContent
-        bottomOutput.textContent = valueToFlip.slice(1, bottomOutput.textContent.length);
-    }
-    else if (bottomOutput.textContent.length < 18 && bottomOutput.textContent != "0") {
-        bottomOutput.textContent = "-" + bottomOutput.textContent;
-    }
+  if (bottomOutput.textContent.includes("-")) {
+    let valueToFlip = bottomOutput.textContent;
+    bottomOutput.textContent = valueToFlip.slice(
+      1,
+      bottomOutput.textContent.length
+    );
+  } else if (
+    bottomOutput.textContent.length < 18 &&
+    bottomOutput.textContent != "0"
+  ) {
+    bottomOutput.textContent = "-" + bottomOutput.textContent;
+  }
 }
 
 function addDecimal() {
-    if (bottomOutput.textContent.length < 18) {
-        bottomOutput.textContent += ".";
-        decimalButton.disabled = true;
-    }
+  if (
+    bottomOutput.textContent.length < 18 &&
+    !bottomOutput.textContent.contains(".")
+  ) {
+    bottomOutput.textContent += ".";
+    decimalButton.disabled = true;
+  }
 }
